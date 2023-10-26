3b6f3a2f124e44df77bec653425d91edf76290c7
balajikrishna1798 <balajikrishna44589@gmail.com>
2023-10-26 22:41:54 +0530

    Contact Section

--- components/ContactSection.jsx
+++ components/ContactSection.jsx
@@ -6,14 +6,39 @@ import Link from "next/link";
 
 const ContactSection = () => {
   return (
-    <div className="flex gap-7 items-center">
-      <Link href={"https://www.linkedin.com/in/balaji-m-b71932284/"}>
-        <Image src={linkedin_icon} width={20} height={20} alt="linkedin"/>
-      </Link>
-      <Link href={"https://github.com/balajikrishna1798/"}>
-        <Image src={github_icon} width={15} height={15} alt="github" className="bg-white"/>
-      </Link>
-    </div>
+    <section className="grid md:grid-cols-2 gap-4 my-2 py-4" id="contact">
+      <div>
+        <h5 className="text-white text-xl font-bold">Contact Details</h5>
+        <div className="text-white"><p><span className="font-bold text-red-500">Phone Number :</span>  8825539047</p><p><span className="font-bold text-orange-300">Email :</span>  balajikrishna44589@gmail.com</p></div>
+        <div className="flex items-center gap-10">
+        <Link href={"https://www.linkedin.com/in/balaji-m-b71932284/"}>
+          <Image src={linkedin_icon}  alt="linkedin" />
+        </Link>
+        <Link href={"https://github.com/balajikrishna1798/"}>
+          <Image
+            src={github_icon}
+           
+            alt="github"
+            className="bg-white"
+          />
+        </Link>
+        </div>
+      </div>
+      <div>
+        <form className="flex flex-col gap-4">
+          <label className="text-white" htmlFor="email">
+            Your email
+          </label>
+          <input
+            type="email"
+            id="email"
+            className="w-9/12 p-1"
+            required
+            placeholder="example@gmail.com"
+          />
+        </form>
+      </div>
+    </section>
   );
 };
 
